342c94689db6dc0caa731b7f09d1eb1c81762eda
liuyao <670652018@qq.com>
2019-03-19 00:31:54 +0800

    fix a bug

--- src/lib-entry.js
+++ src/lib-entry.js
@@ -1,5 +1,6 @@
 import Button from './components/button.vue';
 import ButtonGroup from './components/button-group.vue';
 import Icon from './components/icon.vue';
+import Input from './components/input.vue';
 
-export { Button, ButtonGroup, Icon };
+export { Button, ButtonGroup, Icon, Input };
